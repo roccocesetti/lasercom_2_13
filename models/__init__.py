6d42bf655deb2205b8d9c9021db33f6d286e8b11
roccocesetti <r.cesetti@gmailcom>
2024-05-08 17:04:46 +0200

    committo finali la stampa del contratto formattazione lista province

--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,4 @@ from . import res_partner
 #from . import crm_stage
 #from . import res_users
 from . import sale_order
+from . import res_country_state
--- models/res_country_state.py
+++ models/res_country_state.py
@@ -1,6 +1,14 @@
-from odoo import models, fields, api
+from odoo import models, fields, api,SUPERUSER_ID
 
 class ResCountryState(models.Model):
     _inherit = 'res.country.state'
 
-    venditore_ids = fields.Many2many('res.users', string='Venditori')
+
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        if operator == 'ilike':
+            if SUPERUSER_ID !=self.env.uid and self.env.uid != 2:
+                args =["|",('venditore_ids','in',[self.env.uid]),('venditore_ids','=',False)]
+
+        return super()._name_search(name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid)
+     
