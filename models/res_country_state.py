8e6738c242a04b74ca4ae0bc791eb0eec1b692c2
roccocesetti <r.cesetti@gmailcom>
2024-05-10 18:26:13 +0200

    committo contratto e ricerca con telemarcketer

--- models/res_country_state.py
+++ models/res_country_state.py
@@ -7,7 +7,7 @@ class ResCountryState(models.Model):
     @api.model
     def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
         if operator == 'ilike':
-            if SUPERUSER_ID !=self.env.uid and self.env.uid != 2:
+            if SUPERUSER_ID !=self.env.uid and self.env.uid != 2 and self.env.uid != 10:
                 args =["|",('venditore_ids','in',[self.env.uid]),('venditore_ids','=',False)]
 
         return super()._name_search(name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid)
