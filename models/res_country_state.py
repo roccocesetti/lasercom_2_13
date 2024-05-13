a2641fd44cfe4e19774ff9a3753e4b25b5301845
roccocesetti <r.cesetti@gmailcom>
2024-05-13 10:01:06 +0200

    committo contratto e ricerca con tolto gli stati name_search

--- models/res_country_state.py
+++ models/res_country_state.py
@@ -4,13 +4,13 @@ class ResCountryState(models.Model):
     _inherit = 'res.country.state'
 
 
-    @api.model
-    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
-        if operator == 'ilike':
-            if SUPERUSER_ID !=self.env.uid and self.env.uid != 2 and self.env.uid != 10:
-                args =["|",('venditore_ids','in',[self.env.uid]),('venditore_ids','=',False)]
-
-        return super()._name_search(name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid)
+#    @api.model
+#    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+#        if operator == 'ilike':
+#            if SUPERUSER_ID !=self.env.uid and self.env.uid != 2 and self.env.uid != 10:
+#                args =["|",('venditore_ids','in',[self.env.uid]),('venditore_ids','=',False)]
+#
+#        return super()._name_search(name, args=args, operator=operator, limit=limit, name_get_uid=name_get_uid)
 
 #    @api.model
 #    @api.returns('self',
